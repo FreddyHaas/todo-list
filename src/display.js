f35b85440b012e77ae4a55f090be10ab0390091d
Freddy Haas <email@frederik-haas.de>
2022-11-17 21:24:31 +0100

    Add input buttons for new ToDos

--- src/display.js
+++ src/display.js
@@ -1,25 +1,68 @@
 import dataBase from './dataBase.js'
 
-function renderContent (header) {
+function renderContent () {
     const content = document.getElementById ('content');
-    const contentHeader = document.createElement('p');
-    contentHeader.textContent = `${header}`;
-    content.appendChild(contentHeader);
-    content.appendChild(renderToDos(4, ["test1", "test2", "test3", "test4"])); // Inputs hard copied
+    content.appendChild(createHeader("All")); // Input
+    content.appendChild(createToDoList(4, ["test1", "test2", "test3", "test4"])); // Input
+    content.appendChild(inputButtons());
+    return content;
 }
 
-function renderToDos (number, names) {
-    const toDoContainer = document.createElement ('div');
-    toDoContainer.classList.add('todo-container');
+function createHeader (name) {
+    const contentHeader = document.createElement('p');
+    contentHeader.textContent = `${name}`;
 
+    return contentHeader;
+}
+
+function createToDoList (number, names) {
+    const toDoList = document.createElement('div');
+    toDoList.classList.add('todo-list');
     for (let i = 0; i < number; i++) {
         const toDo = document.createElement('div');
         toDo.textContent = `${names[i]}`;
-        toDo.classList.add('toDo');
-        toDoContainer.appendChild(toDo);
+        toDo.classList.add('todo');
+        toDoList.appendChild(toDo);
     }
-    return toDoContainer;
+    return toDoList;
 }
 
-renderContent("All");
+function inputButtons () {
+    const inputButtons = document.createElement('div');
+    inputButtons.setAttribute('id','todo-button');
+
+    const addButton = document.createElement('div');
+    addButton.classList.add('todo');
+    addButton.textContent = "+ Add ToDo";
+    addButton.setAttribute('id', 'add-task');
+    inputButtons.appendChild(addButton);
+
+    addButton.addEventListener('click', () => {
+        inputButtons.removeChild(inputButtons.lastChild);
+        toDoInputField();
+    });
+
+    return inputButtons;
+}
+
+function toDoInputField () {
+    parent = document.getElementById('todo-button');
+
+    const input = document.createElement('INPUT');
+    input.setAttribute("type","text");
+    input.setAttribute("id", "todo-input");
+    parent.appendChild(input);
+
+    const addButton = document.createElement('button');
+    addButton.setAttribute("id","add-button");
+    addButton.textContent = "Add";
+    parent.appendChild(addButton);
+
+    const cancelButton = document.createElement('button');
+    cancelButton.setAttribute("id", "cancel-button");
+    cancelButton.textContent = "Cancel";
+    parent.appendChild(cancelButton);
+} 
+
+renderContent();
 dataBase();
